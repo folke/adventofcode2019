99bece5ff209278e83e4c2363b34376968a3b9e8
Folke <folke.lemaitre@gmail.com>
2019-12-18 23:46:48 +0100

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -19,18 +19,17 @@ module.exports = {
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
     // collectCoverageFrom: null,
 
     // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
+    coverageDirectory: '.jest',
+    collectCoverageFrom: ['**/*.ts', '!**/node_modules/**'],
 
     // An array of regexp pattern strings used to skip coverage collection
-    // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    coveragePathIgnorePatterns: ['/node_modules/', '/.*.d.ts/'],
 
     // A list of reporter names that Jest uses when writing coverage reports
     // coverageReporters: [
@@ -141,7 +140,7 @@ module.exports = {
     // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    testPathIgnorePatterns: ['/node_modules/', '/lib/']
+    testPathIgnorePatterns: ['/node_modules/', '/build/']
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
