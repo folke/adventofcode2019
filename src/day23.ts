9f55888a74dbca5cdde6834ef6c289ff9479bd78
Folke <folke.lemaitre@gmail.com>
2019-12-23 15:23:20 +0100

    cleanup day 23

--- src/day23.ts
+++ src/day23.ts
@@ -2,7 +2,6 @@ import { Machine, readInput } from './utils'
 
 class Network {
     computers: Machine[] = []
-    queue = new Map<number, [number, number][]>()
     nat: [number, number] = [0, 0]
     part1 = -1
     part2 = -1
@@ -18,45 +17,28 @@ class Network {
     loop() {
         for (let l = 0; l < 1000; l++) {
             let idle = true
-            for (let i = 0; i < 50; i++) {
-                let q = this.queue.get(i)
-                if (q && q.length) {
+            for (const c of this.computers) {
+                if (!c.input.length) c.input.push(-1)
+                c.continue()
+                while (c.output.length >= 3) {
                     idle = false
-                    while (q.length) {
-                        const xy = q.shift()
-                        if (xy) {
-                            this.computers[i].input.push(xy[0])
-                            this.computers[i].input.push(xy[1])
-                            if (i == 0 && xy[1] == this.lastZero) {
-                                this.part2 = xy[1]
-                                return
-                            }
-                            this.lastZero = xy[1]
-                        }
-                    }
-                } else this.computers[i].input.push(-1)
-                this.computers[i].continue()
-                while (this.computers[i].output.length) {
-                    const address = this.computers[i].output.shift() || 0
-                    const x = this.computers[i].output.shift() || 0
-                    const y = this.computers[i].output.shift() || 0
+                    const traffic = c.output.splice(0, 3)
+                    const address = traffic[0]
                     if (address == 255) {
-                        this.nat = [x, y]
-                        if (this.part1 == -1) this.part1 = y
+                        this.nat = [traffic[1], traffic[2]]
+                        if (this.part1 == -1) this.part1 = traffic[2]
                     }
-                    q = this.queue.get(address)
-                    if (!q) {
-                        q = []
-                        this.queue.set(address, q)
+                    if (address == 0 && traffic[2] == this.lastZero) {
+                        this.part2 = traffic[2]
+                        return
                     }
-                    q?.push([x, y])
+                    if (address < 50 && address >= 0) {
+                        this.computers[address].input.push(traffic[1], traffic[2])
+                    }
                 }
-
-                // console.log(this.queue)
             }
             if (idle) {
-                this.computers[0].input.push(this.nat[0])
-                this.computers[0].input.push(this.nat[1])
+                this.computers[0].input.push(...this.nat)
                 if (this.nat[1] == this.lastZero) {
                     this.part2 = this.nat[1]
                     return
